74d01255af7ecbe8d8d799fdac7ad189111dc109
Augusto <Augusto@sim.com>
2026-09-11 12:31:48 -0400

    actualizacion de paquetes .net

--- obj/Debug/net8.0/Korean_Convenience_Store.AssemblyInfo.cs
+++ obj/Debug/net8.0/Korean_Convenience_Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Korean_Convenience_Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a7afff0d1340fb7ffea3b7e718857164366d484")]
 [assembly: System.Reflection.AssemblyProductAttribute("Korean_Convenience_Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Korean_Convenience_Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
