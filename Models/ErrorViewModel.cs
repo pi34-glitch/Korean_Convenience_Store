9a7afff0d1340fb7ffea3b7e718857164366d484
Augusto <Augusto@sim.com>
2026-09-11 11:35:55 -0400

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Korean_Convenience_Store.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
